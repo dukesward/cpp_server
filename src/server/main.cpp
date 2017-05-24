982a401df5130f7a7d25b5c909a564cf3687fab3
Chen <206457840@tfayd.com>
2017-05-24 16:59:53 -0400

    first commit: test cmake

---
+++ src/server/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+#include "common.h"
+#include "config.h"
+
+char *serverName = "mangosr";
+
+extern int main(int argc, char *argv)
+{
+	std::cout << "Hello" << std::endl;
+}

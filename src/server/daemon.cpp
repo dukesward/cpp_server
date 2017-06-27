f2a28c099ab1b08fdcffd658a06ea0617e9825d7
Chen <206457840@tfayd.com>
2017-06-27 16:51:27 -0400

    reconstruction

--- src/server/daemon.cpp
+++
@@ -1,8 +0,0 @@
-
-#ifdef WIN32
-#include "win32.h"
-
-
-
-
-#endif
---
+++ src/server/world/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "Common.h"
+
+extern int main(int argc, char *argv)
+{
+	std::cout << "Hello" << std::endl;
+
+}
